26a11789c9d79574b2726864f59c0b1d4a82e170
luckyaibin@gmail.com <luckyaibin@gmail.com>
2015-09-06 17:15:43 +0800

    ...

---
+++ rh_hashing/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "rhhashing.h"
+
+int main()
+{
+	for (int i=0;i<255;i++)
+	{
+		int v = hash_function(i,255);
+		printf("%d ",v);
+	}
+
+	return 0;
+}
--- rh_hashing/rhhashing.h
+++ rh_hashing/rhhashing.h
@@ -1,6 +1,7 @@
 #ifndef __ROBIN_HASHING_H__
 #define __ROBIN_HASHING_H__
 #include <memory.h>
+#include <stdio.h>
 
 // hash node of hash table
 struct hash_node
@@ -9,9 +10,15 @@ struct hash_node
 	int hash_value;
 };
 
-hash_node* create_hash_table(int size)
+hash_node* create_hash_table(unsigned int size)
 {
 	hash_node *ht = new hash_node[size];
+	for (unsigned i=0;i<size;i++)
+	{
+		hash_node * node = &ht[i];
+		node->val = 0;
+		node->hash_value = -1;//-1 empty, -2 deleted, 0,1,2 ... DIB£¨distance to initial bucket)
+	}
 	memset(ht,0,sizeof(hash_node)*size);
 	return ht;
 }
@@ -19,7 +26,7 @@ hash_node* create_hash_table(int size)
 // hash function 
 int hash_function(int v,int hash_size)
 {
-	int v = v + v << 5;
+	v = v*hash_size + (v << 5);
 	v = v % hash_size;
 	return v;
 }
@@ -28,6 +35,9 @@ int insert(hash_node * ht,int size,int val)
 {
 	int hv = hash_function(val,size);
 
+	if (ht[hv].hash_value )
+	{
+	}
 
 	return hv;
 }
