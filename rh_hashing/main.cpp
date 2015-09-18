39b5b5e72b29eba2d773a49ea6ff7386cb077e1b
ab <ln@g>
2015-09-18 22:31:22 +0800

    ...

--- rh_hashing/main.cpp
+++ rh_hashing/main.cpp
@@ -3,7 +3,7 @@
 #include<map>
 using namespace  std;
 multimap<int,int> mm;
-
+typedef map<int,int> IntMapType;
 
 int g_serial_id =0;
 int main()
@@ -23,30 +23,30 @@ int main()
 	/*
 	[8,100] [1,101] [1,102] [1,103] [2,104] [4,105] [-2,106]   [5,107]	[5,5]   [8,109]
 	*/
-	for (int i=0;i<10;i++)
+	/*for (int i=0;i<10;i++)
 	{
-		int v = hash_function(i,10);
-		printf("[%d]=%d,\n ",i,v);
-	}
+	int v = hash_function(i,10);
+	printf("[%d]=%d,\n ",i,v);
+	}*/
 	int hash_table_size = 10;
-	hash_table * ht = create_hash_table(hash_table_size);
-
-	dump_hash_table(ht);
-
-	//			 0	1	2	3	4	5	6	7	8	9
-	//int arr[] = {8,	1,	1,	1,	2,	4,	-4,	5,	-8,	8};
-	//	dib		 2	0	1	2	2	1	8	2	9	1
-	for (int i=0;i<10;i++)
+	hash_table * ht = create_hash_table();
+	IntMapType imt;
+	for (int i=0;i<250;i++)
 	{
-		ht->hn[i].hash_value = arr[i];
-		ht->hn[i].val = 100+i;
+		int v = rand() % 200;
+		//imt[v]=v;
+		imt.insert(std::make_pair(v,v));
+		rhht_unique_insert(ht,v);
 	}
-	dump_hash_table(ht);
-
-
-	rhht_insert(ht,5);
+	 
 	
 	dump_hash_table(ht);
+
+	printf("\n\n");
+	for (IntMapType::iterator it = imt.begin();it!=imt.end();it++)
+	{
+		printf("%d ,",it->second);
+	}
 
 	//int index = __rhht_find(ht,7);
 	//index = __rhht_find(ht,6);
